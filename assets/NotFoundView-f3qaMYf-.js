2f68459943c3c9db63bd47ffb45fc3c252fd9551
Cultizm <Liks-ea@mail.ru>
2024-08-26 23:38:34 +0300

    dist

---
+++ assets/NotFoundView-f3qaMYf-.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as e}from"./index-DLW0Bkq-.js";const s={setup(){return{}}},c={class:"mb-3 mr-auto text-[36px] font-bold"};function n(r,a,_,p,u,d){return o(),e("h1",c,"Упс, такой страницы нет")}const i=t(s,[["render",n]]);export{i as default};
